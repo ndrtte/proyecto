656a31375f1c66d2e65d0d331d28871aec86226e
ndrtte <andreamorelaleman@gmail.com>
2024-11-30 14:11:14 -0600

    PrestamoController

--- src/main/java/hn/unah/proyecto/controladores/PrestamoController.java
+++ src/main/java/hn/unah/proyecto/controladores/PrestamoController.java
@@ -1,10 +1,47 @@
 package hn.unah.proyecto.controladores;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import hn.unah.proyecto.dtos.PrestamosDTO;
+import hn.unah.proyecto.servicios.PrestamoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/prestamo")
+@RequestMapping("/api/prestamos")
 public class PrestamoController {
 
+    @Autowired
+    private PrestamoServicio prestamoServicio;
+
+    @PostMapping
+    public String crearPrestamo(@RequestBody PrestamosDTO prestamo) {
+        return prestamoServicio.crearPrestamo(prestamo);
+    }
+
+    @GetMapping("/dni/{dni}")
+    public List<PrestamosDTO> buscarPrestamosPorDni(@PathVariable String dni) {
+        return prestamoServicio.buscarPrestamosPorDni(dni);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<PrestamosDTO> buscarPrestamoPorId(@PathVariable int id) {
+        return prestamoServicio.buscarPrestamoPorId(id);
+    }
+
+    @PutMapping("/{idPrestamo}/{dniCliente}")
+    public void asociarPrestamoACliente(@PathVariable int idPrestamo, @PathVariable String dniCliente) {
+        prestamoServicio.asociarPrestamoACliente(idPrestamo, dniCliente);
+    }
+
+    @GetMapping("/saldo/{idPrestamo}")
+    public double obtenerSaldoPendiente(@PathVariable int idPrestamo) {
+        return prestamoServicio.obtenerSaldoPendiente(idPrestamo);
+    }
+
+    @PutMapping("/pagar/{idPrestamo}")
+    public void pagarCuota(@PathVariable int idPrestamo) {
+        prestamoServicio.pagarCuota(idPrestamo);
+    }
 }
